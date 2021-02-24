bbedf1e464079505bc2ac4b991fcf4fa878d6ee9
FlutLab.io <>
2021-02-24 08:32:21 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -19,24 +19,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-
-  const MyHomePage({@required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
-  }
-}
